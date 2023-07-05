1e70e3acb3ea8eeaa9f4c8428e4faf8467dfc1be
Gyanesh Pandey <gyanesh.pandey@gmail.com>
2023-07-05 09:01:44 -0400

    File to return disk usage based checks

---
+++ all_checks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import sys
+
+def check_reboot():
+    """Returns true if computer has a pending reboot. """
+    print("in check_reboot")
+    return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns true if there isn't enough disk space, False otherwise"""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    precentage_free = 100*du.free/du.total
+    # Calculate free space in GB
+    gb_free = du.free/2**30
+    if precentage_free < min_percent or gb_free < min_gb:
+        return True
+    return False
+
+def main():
+    if check_reboot():
+        print("Pending reboot.")
+        sys.exit(1)
+    if check_disk_full(disk="/",min_gb= 2,min_percent= 10):
+        print("Disk Full.")
+        sys.exit(1)
+
+main()
